e9900d30b734baf49cb4190c7ed191ff6f11e49a
Roger Melo <rogerw592@gmail.com>
2019-09-14 14:35:09 -0300

    Improved challenge Description

--- challenges/01-entendendo-o-switch-case/challenge-02.js
+++ challenges/01-entendendo-o-switch-case/challenge-02.js
@@ -2,8 +2,8 @@ const getDay = () => Math.floor(Math.random() * 7) + 1
 const showMessage = (day) => console.log(`The day is ${day}`)
 
 /*
-Agora, organize os dias, dividindo-os entre dias da semana e fim de
-semana.
+Agora, à partir do código que você escreveu, organize os dias,
+dividindo-os entre dias da semana e fim de semana.
 
 As mensagens a serem exibidas no console devem ser:
 
